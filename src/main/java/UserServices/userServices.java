1ec854960de883ef91c27de74cb6e2eff7b631de
Charles <2103602217@qq.com>
2023-01-01 20:40:57 +0800

    用户信息POJO类初步设定

---
+++ src/main/java/UserServices/userServices.java
@@ -0,0 +1,36 @@
+package UserServices;
+
+/**
+ * 用户类
+ */
+public class userServices {
+    /**
+     * 唯一ID，自动生成唯一ID
+     */
+    Long userID;
+    /**
+     * 姓名
+     */
+    String name;
+    /**
+     * 年龄
+     */
+    Integer age;
+    /**
+     * 性别
+     */
+    String gender;
+    /**
+     * 家庭住址
+     */
+    String address;
+    /**
+     * 户主
+     */
+    Long houseHolder;
+    /**
+     * 电话
+     */
+    String phone;
+
+}
