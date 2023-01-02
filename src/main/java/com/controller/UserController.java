a6a7f7298aa8eac330544be105706c45c11b6411
Charles <2103602217@qq.com>
2023-01-02 20:50:22 +0800

    fix some bug with a low version maven,and add some file of UserServices.

---
+++ src/main/java/com/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.controller;
+
+/**
+ * @Author：Charles
+ * @Package：com.controller
+ * @Project：EpidemicPreventionAndControl
+ * @name：UserController
+ * @Date：2023/1/2 20:47
+ * @Filename：UserController
+ */
+public class UserController {
+}
---
+++ src/main/java/com/services/UserServices.java
@@ -0,0 +1,12 @@
+package com.services;
+
+/**
+ * @Author：Charles
+ * @Package：com.services
+ * @Project：EpidemicPreventionAndControl
+ * @name：UserServices
+ * @Date：2023/1/2 20:48
+ * @Filename：UserServices
+ */
+public class UserServices {
+}
