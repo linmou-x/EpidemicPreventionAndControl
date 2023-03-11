ac8decefc84740d98579b5d9e963b6f30a71ee06
Charles <2103602217@qq.com>
2023-03-11 16:22:30 +0800

    hutoolall

--- src/main/java/com/services/FileServiceImpl.java
+++ src/main/java/com/services/FileServiceImpl.java
@@ -1,8 +1,14 @@
 package com.services;
 
+import cn.hutool.core.io.FileTypeUtil;
+import com.entity.User;
 import com.services.Impl.FileService;
 import com.utils.Result;
+import com.utils.ResultEnum;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.InputStream;
+import java.util.List;
 
 /**
  * @Author：Charles
@@ -15,11 +21,16 @@ import org.springframework.web.multipart.MultipartFile;
 public class FileServiceImpl implements FileService {
     /**
      * 上传Excel
-     * @param multipartFile
+     * @param file
      * @return
      */
     @Override
-    public Result uploadExcel(MultipartFile multipartFile) {
+    public Result uploadExcel(MultipartFile file) {
+        String isExcel= FileTypeUtil.getType((InputStream) file);
+        if (isExcel!="excel"){
+            return new Result(ResultEnum.FAIL,"文件类型不为Excel");
+        }
+//        List<User> userList=excelUtil.simpleExcelRead(file,User.class);
         return null;
     }
 }
