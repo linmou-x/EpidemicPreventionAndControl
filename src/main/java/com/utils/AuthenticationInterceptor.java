092953a6840ed87ae25720cdb30370147471945e
charles <21036022217@qq.com>
2023-03-18 16:18:44 +0800

    添加token解析注释

--- src/main/java/com/utils/AuthenticationInterceptor.java
+++ src/main/java/com/utils/AuthenticationInterceptor.java
@@ -17,19 +17,30 @@ public class AuthenticationInterceptor implements HandlerInterceptor {
     UserService userService;
 
     @Resource
-    Token JWTtoken;
+    Token JwtToken;
     @Override
     public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object object) throws Exception {
+        /**
+         * 从httpServletRequest中获取到token，
+         */
         String token = httpServletRequest.getHeader("token");
         logger.debug("获取token:",token);
+        /**
+         * 非访问接口直接跳过
+         */
         if(!(object instanceof HandlerMethod)){
             logger.debug("直接跳过");
             return true;
         }
+        /**
+         * 获取接口方法名
+         */
         HandlerMethod handlerMethod=(HandlerMethod)object;
         Method method=handlerMethod.getMethod();
         logger.debug("方法名:",String.valueOf(method));
-        //检查是否有passtoken注释，有则跳过认证
+        /**
+         *  检查是否有@passtoken注释，有则跳过认证
+         */
         if (method.isAnnotationPresent(PassToken.class)) {
             PassToken passToken = method.getAnnotation(PassToken.class);
             if (passToken.required()) {
@@ -37,27 +48,28 @@ public class AuthenticationInterceptor implements HandlerInterceptor {
                 return true;
             }
         }
-        //检查有没有需要用户权限的注解
+        /**
+         * 检查是否有@UserLoginToken 有则对token进行解析
+         */
         logger.debug("method 注解"+String.valueOf(method.isAnnotationPresent(UserLoginToken.class)));
         if (method.isAnnotationPresent(UserLoginToken.class)) {
             logger.debug("进去方法需要注解");
             UserLoginToken userLoginToken = method.getAnnotation(UserLoginToken.class);
+            /**
+             * required 方法默认为true，即需要
+             */
             if (userLoginToken.required()) {
-                // 执行认证
+
                 if (token==null) {
                     logger.debug("无Token,跳过验证:");
                     throw new RuntimeException("无token，请重新登录");
                 }
-                if (!JWTtoken.verifyToken(token)){
+                /**
+                 * 调用token工具类验证token可用
+                 */
+                if (!JwtToken.verifyToken(token)){
                     throw new RuntimeException("Token过期失效");
                 }
-                // 获取 token 中的 user id
-//                try {
-//
-//                    JWTtoken.getId(token);
-//                } catch (JWTDecodeException j) {
-//                    throw new RuntimeException("401");
-//                }
                 return true;
             }
         }
@@ -70,6 +82,15 @@ public class AuthenticationInterceptor implements HandlerInterceptor {
                            Object o, ModelAndView modelAndView) throws Exception {
 
     }
+
+    /**
+     * preHandle完成操作
+     * @param httpServletRequest
+     * @param httpServletResponse
+     * @param o
+     * @param e
+     * @throws Exception
+     */
     @Override
     public void afterCompletion(HttpServletRequest httpServletRequest,
                                 HttpServletResponse httpServletResponse,
