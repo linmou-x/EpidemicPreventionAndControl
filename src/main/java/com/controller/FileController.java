148f7afbaeb22805c6b62b830424f51a444b97e9
charles <21036022217@qq.com>
2023-04-01 20:06:45 +0800

    。。。。

--- src/main/java/com/controller/FileController.java
+++ src/main/java/com/controller/FileController.java
@@ -2,6 +2,8 @@ package com.controller;
 
 import com.services.Impl.FileService;
 import com.utils.Result;
+import com.utils.ResultEnum;
+import org.apache.logging.log4j.message.Message;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,7 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
 import java.io.IOException;
+import java.util.UUID;
 
 /**
  * @Author：Charles
