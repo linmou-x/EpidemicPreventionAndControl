edc8efd87e8f172d2521223a38c22788b2ec56bb
Charles <2103602217@qq.com>
2023-04-12 16:26:20 +0800

    za

--- src/main/java/com/entity/GoodDTO.java
+++ src/main/java/com/entity/GoodDTO.java
@@ -1,14 +1,11 @@
 package com.entity;
 
-import com.baomidou.mybatisplus.annotation.*;
-import com.fasterxml.jackson.annotation.JsonFormat;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
-
 import java.io.Serializable;
-import java.util.Date;
 
 @Data
 @AllArgsConstructor
